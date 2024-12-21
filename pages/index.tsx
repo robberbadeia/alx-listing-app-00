df5bf951aad700c5b56c5239ca6297a0b45df7b8
robberbadeia <robber.badeia@gmail.com>
2024-12-21 07:05:49 -0800

    Adjust project global style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  weight: "100 900",
 });
-
-const geistMono = Geist_Mono({
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: "100 900",
 });
 
 export default function Home() {
