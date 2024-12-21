9fc56351d950e4452a2fec0d77833a4d45276086
robberbadeia <robber.badeia@gmail.com>
2024-12-21 07:04:35 -0800

    Create PropertyProps interface

---
+++ interfaces/index.ts
@@ -0,0 +1,20 @@
+interface CardProps {}
+interface ButtonProps {}
+interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  image: string;
+  discount: string;
+}
